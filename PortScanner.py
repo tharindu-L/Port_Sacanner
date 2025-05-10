704596dadb932459c7ebd035e4ee4efdefeb25ab
ThArInDu0621 <lakmal8442tharindu@gmail.com>
2025-05-11 05:24:59 +0530

    Add threadings

--- PortScanner.py
+++ PortScanner.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 ip = (input("Enter the IP address or domain name : "))
 start_port = int(input("Enter the start port : "))
@@ -6,6 +7,22 @@ end_port = int(input("Enter the end port : "))
 
 target_ip = socket.gethostbyname(ip)
 
+common_ports = {
+    20: "FTP (Data)",
+    21: "FTP (Control)",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    445: "SMB",
+    3306: "MySQL",
+    3389: "RDP",
+    8080: "HTTP-Proxy",
+}
 
 def scan_port(ip, port):
     try:
@@ -13,15 +30,24 @@ def scan_port(ip, port):
         s.settimeout(1)
         result = s.connect_ex((ip, port))
         if result == 0:
-            print(f"[+] Port {port} is open")
+            service = common_ports.get(port, "Unknown Service")
+            print(f"[+] Port {port} is open - {service}\n")
         else:
-            print(f"[-] Port {port} is closed")
+            print(f"[-] Port {port} is closed\n")
         s.close()
     except Exception as e:
         print(f"[!] Error scanning port {port}: {e}")
 
+def threaf_scan(ip, port):
+    scan_port(ip, port)
 
+threads = []
 print(f"Scanning {target_ip} from port {start_port} to port {end_port}\n")
 
 for port in range(start_port, end_port + 1):
-    scan_port(target_ip, port)
+    t = threading.Thread(target=threaf_scan, args=(target_ip, port))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
